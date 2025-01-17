a0a1aff721e570eb5d6f3b875108a128fd85a45b
Moritz Laupichler <molaupi@googlemail.com>
2025-01-17 12:01:00 +0100

    Cleanup for publication.

--- README.md
+++ README.md
@@ -1,11 +1,47 @@
 # Experimental Implementation of Synergistic Traffic Assignment (STA)
 
-To compute an STA equilibrium using this code, compile (using GCC) and execute (main function in cch/main.cpp).
-The code expects input files in <project-root>/inputs. 
+This is an experimental implementation of the Synergistic Traffic Assignment (STA) algorithm written in C++ 17.
+For information on the algorithm, we refer to the following paper:
+
+* Thomas Bläsius, Adrian Feilhauer, Markus Jung, Moritz Laupichler, Peter Sanders, Michael Zündorf. 
+  2025. 
+  Synergistic Traffic Assignment. 
+  In Proc. of the 24th International Conference on Autonomous Agents and Multiagent Systems (AAMAS 2025), 
+  Detroit, Michigan, USA, May 19 – 23, 2025, 
+  IFAAMAS
+  (to be published)
+
+If you use this code in your scientific publication, we ask that you cite the paper above. 
+
+## License
+All files in this repository are licensed under the MIT license.
+
+## Building
+To build the code, you need a modern C++ compiler.
+For instance, using GCC (version 11.4.0 or later), run the following command at the top-level directory:
+```
+g++ -O3 cch/main.cpp -o sta
+``` 
+
+## Input data
+The code expects input files to be placed in the `inputs` directory.
 The following input files are expected (all in [RoutingKit](https://github.com/RoutingKit/RoutingKit) vector format):
 * a road network given as an adjacency array consisting of index array `first_out` (one entry per vertex) and edge head array `head` (one entry per edge)
 * travel times `travel_time` (one value per edge)
-* a CCH node order `cch_order` (one value per vertex); this can be computed, e.g., using [InertialFlowCutter](https://github.com/kit-algo/InertialFlowCutter)
-* O-D pairs as two arrays of vertex IDs `od_pairs/<basename>.origin` and `od_pairs/<basename>.destination`(one entry per O-D pair each); <basename> can be specified as a command line argument to the compiled program
+* a CCH node order `cch_order` (one value per vertex)
+* O-D pairs as two arrays of vertex IDs `od_pairs/<basename>.origin` and `od_pairs/<basename>.destination`(one entry per O-D pair each); `<basename>` can be specified as a command line argument to the compiled program, see below
 
-The selfishness parameter `r` is hardcoded (at the top of main.cpp) and can be changed before compiling. The default value is `r=0`. 
+RoutingKit offers [functionality](https://github.com/RoutingKit/RoutingKit/blob/master/doc/OpenStreetMap.md) for generating road networks in the desired format using OSM data.
+[InertialFlowCutter](https://github.com/kit-algo/InertialFlowCutter) is able to compute a CCH node order for a given road network in RoutingKit format.
+
+## Running
+To execute the STA algorithm, run the following command:
+```
+./sta <basename> <selfishness>
+```
+
+The first command line parameter `<basename>` refers to the name of the O-D pair files (see above).
+The second command line parameter `<selfishness>` is the selfishness tuning parameter (explained in the paper). 
+This must be a decimal number between `0.0` and `1.0`.
+
+The program prints statistics on each iteration of the algorithm in standard out and places a file containing the final flow pattern in the `outputs` directory (one value per edge in RoutingKit vector format). 
--- cch/main.cpp
+++ cch/main.cpp
@@ -4,34 +4,45 @@
 
 using namespace std::chrono;
 
-constexpr double RATIO = 0.0;
-constexpr double DIV = 1.0;//1.0;
-constexpr inline weight_t cost(flow_t flow) {
-	return RATIO + (1.0 - RATIO) / ((flow / DIV) + 1);
+inline double cost(flow_t flow, double selfishness) {
+	return selfishness + (1.0 - selfishness) / (flow + 1);
 }
 
 int main(int argc, char** argv) {
-	assert(argc == 2);
+	if (!argc == 3) {
+		std::cerr << "Expects exactly 2 command line parameters, " 
+					 "the name of the demand data, and the selfishness parameter."
+				  << std::endl;
+		exit(-1);
+	}
+
+	// Read name of demand data
 	std::filesystem::path file(argv[1]);
 	std::string name = file.stem();
 	std::string basename = name.substr(0, name.find('.'));
 	assert(!name.empty());
 
+	// Read selfishness parameter
+	double selfishness = 0.0;
+	try {
+    	selfishness = std::stod(argv[2]);
+	} catch (std::exception const &e) {
+    	std::cerr << "Could not parse selfishness parameter." << std::endl;
+    	exit(-1);
+	} 
+
 	std::cout << std::fixed << std::setprecision(6);
 	std::cerr << std::fixed << std::setprecision(6);
 
-	std::vector<edge_id> first_out = loadVector<edge_id>("../inputs/first_out");
-	std::vector<node_id> head = loadVector<node_id>("../inputs/head");
-	std::vector<uint32_t> travel_time = loadVector<uint32_t>("../inputs/travel_time");
+	std::vector<edge_id> first_out = loadVector<edge_id>("./inputs/first_out");
+	std::vector<node_id> head = loadVector<node_id>("./inputs/head");
+	std::vector<uint32_t> travel_time = loadVector<uint32_t>("./inputs/travel_time");
 	std::vector<weight_t> dist(travel_time.size());
 	for (edge_id i = 0; i < travel_time.size(); i++) dist[i] = static_cast<weight_t>(travel_time[i]);
-	// std::vector<float> lat = loadVector<float>("../inputs/latitude");
-	// std::vector<float> lng = loadVector<float>("../inputs/longitude");
-	std::vector<node_id> node_order = loadVector<node_id>("../inputs/cch_order");
-	// std::vector<capacity_t> capacity = loadVector<capacity_t>("../inputs/capacity");
+	std::vector<node_id> node_order = loadVector<node_id>("./inputs/cch_order");
 
-	std::vector<node_id> origin = loadVector<node_id>("../inputs/od_pairs/" + basename + ".origin");
-	std::vector<node_id> destination = loadVector<node_id>("../inputs/od_pairs/" + basename + ".destination");
+	std::vector<node_id> origin = loadVector<node_id>("./inputs/od_pairs/" + basename + ".origin");
+	std::vector<node_id> destination = loadVector<node_id>("./inputs/od_pairs/" + basename + ".destination");
 	assert(origin.size() == destination.size());
 
 	std::cout << "Graph size: " << node_order.size() << " " << head.size() << ", O-D-Pairs: " << origin.size() << std::endl;
@@ -54,9 +65,9 @@ int main(int argc, char** argv) {
 	/* only used to calculate potential
 	double last_potential = -1;
 	std::vector<double> accumulated_cost(origin.size() + 1);
-	accumulated_cost[0] = cost(0);
+	accumulated_cost[0] = cost(0, selfishness);
 	for (std::size_t i = 1; i < accumulated_cost.size(); i++) {
-		accumulated_cost[i] = accumulated_cost[i - 1] + cost(i);
+		accumulated_cost[i] = accumulated_cost[i - 1] + cost(i, selfishness);
 	}*/
 
 
@@ -80,7 +91,7 @@ int main(int argc, char** argv) {
 		std::cout << "  calculate flow/new travel time" << std::endl;	
 		//double potential = 0;
 		for (edge_id i = 0; i < cch.input_arc_count(); i++) {
-			new_dist[i] = dist[i] * cost(flow[i]);
+			new_dist[i] = dist[i] * cost(flow[i], selfishness);
 			assert(new_dist[i] <= dist[i]);
 			//potential += accumulated_cost[flow[i]];
 		}
@@ -88,15 +99,13 @@ int main(int argc, char** argv) {
 		std::cerr << iteration << "\t" << total << "\t" << potential << std::endl;
 		last_potential = potential;*/
 
-		//dumpVector<flow_t>("../outputs/" + basename + ".ratio." + std::to_string(RATIO) + ".div." + std::to_string(DIV) + "." + std::to_string(iteration) + ".flow", flow);
-
 		std::cout << "  preparing next iteration" << std::endl;
 		metric.customize(new_dist, true);
 	} while (true);
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
 
-	//dumpVector<flow_t>("../outputs/" + basename + ".cap.flow", flow);
+	dumpVector<flow_t>("./outputs/" + basename + ".ratio." + std::to_string(selfishness) + ".iteration." + std::to_string(iteration) + ".flow", flow);
 
 	std::cout << "iterations: " << iteration << std::endl;
 	std::cout << "total: " << duration.count() << " microseconds" << std::endl;
